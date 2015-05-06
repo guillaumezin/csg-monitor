d14192c3c4db38bde9d8ffb5232fb27f986280b9
Matt Mombrea <mombrea@cypressnorth.com>
2015-05-06 12:05:42 -0400

    Add functions for Raspberry Pi monitoring

--- monitor.py
+++ monitor.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # many ideas stolen from
 # https://github.com/kylemarkwilliams/website-monitor
 
@@ -5,6 +6,10 @@ import requests
 import smtplib
 import time
 import thread
+import pibrella
+import socket
+import os.path
+import math 
 
 from datetime import datetime
 from email.MIMEMultipart import MIMEMultipart
@@ -14,9 +19,6 @@ from repeattimer import RepeatTimer
 
 from monitor_config import monitor_config
 from servers import server_list
-
-# interval = monitor_config['interval']
-# recipients = monitor_config['recipients']
 
 args = []
 kwargs = {}
@@ -30,10 +32,10 @@ def mail(to, subject, text):
     msg.attach(MIMEText(text))
     mailServer = smtplib.SMTP(monitor_config['mail_server'],
                               monitor_config['mail_server_port'])
-    # mailServer.ehlo()
-    # mailServer.starttls()
-    # mailServer.ehlo()
-    # mailServer.login(monitor_config['mail_user'], monitor_config['mail_pass'])
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(monitor_config['mail_user'], monitor_config['mail_pass'])
     mailServer.sendmail(monitor_config['mail_user'], to, msg.as_string())
     mailServer.close()
 
@@ -41,13 +43,23 @@ def mail(to, subject, text):
 class Monitor(object):
 
     def __init__(self, server_list, monitor_config):
-        print "__init__"
+        print ("__init__")
         self.interval = monitor_config['interval']
         self.recipients = monitor_config['recipients']
         self.servers = self.get_servers(server_list)
+        self.learn_ip()
+        pibrella.button.pressed(self.btnPress)
+
+    def learn_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("gmail.com",80))
+        ip = s.getsockname()[0]
+        s.close()
+        for recipient in self.recipients:
+            mail(recipient, 'Pi Monitor Started Successfully','Pi Monitor initialized on ip: %s' % (ip))
 
     def run(self):
-        print "run: "
+        print ("run: ")
         repeat_timer = RepeatTimer(self.interval,
                                    self.check_servers,
                                    *args,
@@ -56,25 +68,28 @@ class Monitor(object):
 
     def check_servers(self, *args, **kwargs):
         """"""
-        print "check_servers: "
+        print ("check_servers: ")
         for server in self.servers:
-            print server.name
+            print (server.name)
             thread.start_new_thread(server.check_status, ())
         # email message about down servers
         time.sleep(5)
         down_servers = self.get_down_servers()
         if len(down_servers) > 0:
             self.send_down_servers_email(down_servers)
+        else:
+            self.reset()
 
     def get_down_servers(self):
         down_servers = []
         for server in self.servers:
-            if server.status != 'OK' and server.fails >= server.max_fails:
+            if server.status != 'OK' and server.fails >= server.max_fails and server.notified_fail == False:
                 down_servers.append(server)
         return down_servers
 
     def send_down_servers_email(self, down_servers):
-        print "send_down_servers_email"
+        self.alarm()
+        print ("send_down_servers_email")
         message = ''
         for server in down_servers:
             text = "%s %s %s - %s\n" % (server.name,
@@ -82,12 +97,13 @@ class Monitor(object):
                                         server.url,
                                         server.status)
             message += text
+            server.notified_fail = True
         for recipient in self.recipients:
-            mail(recipient, 'CSG Monitor', message)
+            mail(recipient, 'Pi Monitor', message)
 
     def get_servers(self, server_list):
         """takes list of dicts and return list of Server objects"""
-        print "get_servers: "
+        print ("get_servers: ")
         servers = []
         for server in server_list:
             servers.append(Server(name=server['name'],
@@ -97,6 +113,22 @@ class Monitor(object):
                                   assert_string=server['assert_string']))
         return servers
 
+    def btnPress(self,pin):
+        self.reset()
+        time.sleep(3600)
+	
+    def reset(self, pin):
+        pibrella.light.stop()
+        pibrella.buzzer.stop()
+        for server in self.servers:
+            server.notified_fail = False
+            server.fails = 0
+            server.status = 'OK'
+            server.assert_pass = True
+
+    def alarm(pin):
+        pibrella.light.pulse()
+        pibrella.buzzer.buzz(50)
 
 class Server:
 
@@ -142,13 +174,10 @@ class Server:
                 self.fails += 1
                 self.status = 'ERROR'
 
-        print self.name, self.status
+        print (self.name, self.status)
         return self
 
 if __name__ == "__main__":
     # need to update server list when necessary
     monitor = Monitor(server_list, monitor_config)
     monitor.run()
-    # servers = get_servers(server_list)
-    # repeat_timer = RepeatTimer(interval, check_servers, *args, **kwargs)
-    # repeat_timer.start()
