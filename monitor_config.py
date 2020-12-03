04c92a23f0f4e5887680db82b1368095c3321c44
guillaumezin <guillaume.zin@gmail.com>
2020-12-03 21:14:42 +0100

    Restore default parameters

--- monitor_config.py
+++ monitor_config.py
@@ -1,10 +1,10 @@
 monitor_config = {
     'interval': 300,
     'heartbeatFile': '/var/lib/sitemon/heartbeat.txt',
-    'heartbeatHours': 720,
-    'recipients': ['guillaume.zin@gmail.com'],
-    'mail_user': 'guillaume.zin@gmail.com',
-    'mail_pass': 'etfqjesdgyjffcwm',
+    'heartbeatHours': 24,
+    'recipients': ['email@server.com'],
+    'mail_user': 'email@server.com',
+    'mail_pass': 'mypassword',
     'mail_server': 'smtp.gmail.com',
     'mail_server_port': 587
 }
--- servers.py
+++ servers.py
@@ -1,12 +1,12 @@
 server_list = [
-    {'name': 'StJean',
-     'url': 'https://choletstjean.freeddns.org',
+    {'name': 'Google',
+     'url': 'https://google.com',
      'timeout': 300,
      'max_fails': 2,
-     'assert_string': 'Please Login'},
-    {'name': 'SteFamille',
-     'url': 'https://choletstefamille.freeddns.org',
+     'assert_string': 'Google Inc.'},
+    {'name': 'Bing',
+     'url': 'http://www.bing.com',
      'timeout': 300,
      'max_fails': 2,
-     'assert_string': 'Please Login'}
+     'assert_string': 'Make Bing my homepage'}
 ]
